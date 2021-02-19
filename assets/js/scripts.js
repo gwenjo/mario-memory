4ffe602c9ee165692202135eaeedd1ebb71b09bc
gwenjo <jo.gwen@hotmail.com>
2021-02-19 23:12:38 +0000

    changed back js fixed errors

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,18 @@
-let timerOn = true;
+//Code taken from https://github.com/taniarascia/memory/blob/master/js/script.original.js
+
+var firstGuess = "";
+var secondGuess = "";
+var previousTarget = null;
+var delay = 1800;
+var timerOn = true;
 
 //Code taken from https://github.com/code-sketch/memory-game/blob/master/video-11/scripts.js
 
 const cards = document.querySelectorAll(".memory-card");
 
-let hasFlippedCard = false;
-let lockBoard = false;
-let firstCard, secondCard;
+var hasFlippedCard = false;
+var lockBoard = false;
+var firstCard, secondCard;
     
 function flipCard() {
     if (lockBoard) return;
@@ -25,17 +31,19 @@ function flipCard() {
 }
 
 function checkForMatch() {
-    if (firstCard.dataset.frame === secondCard.dataset.frame) {
+  if (firstCard.dataset.frame === secondCard.dataset.frame) {
     disableCards();
     return;
-    }
-    unflipCards();
+  }
+
+  unflipCards();
 }
 
 function disableCards() {
-    firstCard.removeEventListener("click", flipCard);
-    secondCard.removeEventListener("click", flipCard);
-    resetBoard();
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+
+  resetBoard();
 }
 
 function unflipCards() {
@@ -45,36 +53,47 @@ function unflipCards() {
         firstCard.classList.remove("flip");
         secondCard.classList.remove("flip");
 
-        resetBoard();
-    },  1800);
+      resetBoard();
+    }, 1800);
 }
 
 function resetBoard() {
-    [hasFlippedCard, lockBoard] = [false, false];
-    [firstCard, secondCard] = [null, null];
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
 }
 
 (function shuffle() {
-    cards.forEach((card) => {
-    let ramdomPos = Math.floor(Math.random() * 12);
+  cards.forEach((card) => {
+    var ramdomPos = Math.floor(Math.random() * 12);
     card.style.order = ramdomPos;
-    });
+  });
 })();
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
 
 // code taken from https://www.taniarascia.com/how-to-create-a-memory-game-super-mario-with-plain-javascript/
-//timer
-let time = 0;
-let timer;
+
+var time = 0;
+var timer;
 function startTimer() {
     timer = setInterval(function () {
     time++;
-    }, 1000);
+    minutes = ("0" + Math.floor(time / 60)).slice(-2);
+    seconds = ("0" + (time % 60)).slice(-2);
+    document.querySelector(".timer").innerHTML = minutes + ":" + seconds;
+  }, 1000);
 }
 
+var clicked = flipCard;
 if (timerOn === true) {
     startTimer();
     timerOn = true;
 }
- 
+
+function reloadGame() {
+  window.location.reload();
+}
+
+var startGame = function myFunction() {
+  reloadGame();
+};
