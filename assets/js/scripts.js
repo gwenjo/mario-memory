0478f70389b5b0005ce075823e4a9bea1377127d
gwenjo <jo.gwen@hotmail.com>
2021-02-25 00:19:26 +0000

    changed layout en removed timer from all pages

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -40,7 +40,7 @@ function disableCards() {
     firstCard.removeEventListener("click", flipCard);
     secondCard.removeEventListener("click", flipCard);
 
-  resetBoard();
+  reset();
 }
 
 function unflipCards() {
@@ -48,17 +48,14 @@ function unflipCards() {
     setTimeout( //used to keep the cards visiable for short time
     () => {
         firstCard.classList.remove("flip");
+
         secondCard.classList.remove("flip");
 
-      resetBoard();
+      reset();
     }, 
     1300);
 }
 
-function resetBoard() {
-  [hasFlippedCard, lockBoard] = [false, false];
-  [firstCard, secondCard] = [null, null];
-}
 
 (function shuffle() {
   cards.forEach((card) => {
@@ -67,33 +64,41 @@ function resetBoard() {
   });
 })();
 
+
+// Reset the game, new game
+function reset() {
+    setTimeout(() => {
+        [hasFlippedCard, lockBoard] = [false, false];
+        [firstCard, secondCard] = [null, null];
+        card.forEach(cardReset => cardReset.classList.remove('flip'));
+        card.forEach(cardBg => cardBg.classList.remove('bg_green'));
+        // Remove the red background in that case when the user start a new game
+        // while a not matched pair is stil not flipped back.  
+        card.forEach(cardBg => cardBg.classList.remove('bg_red'));
+        shuffle();
+        card.forEach(cards => cards.addEventListener('click', flipOver));
+        }, 800);
+        // For the case the player don't close the game finished window.
+        closeWindow();  
+    }
+
+
 cards.forEach((card) => card.addEventListener("click", flipCard));
 
-// code taken from https://www.taniarascia.com/how-to-create-a-memory-game-super-mario-with-plain-javascript/
-
-let time = 0;
-let timer;
-
-function beginTimer() {
-    timer = setInterval(function () {
-    time++;
-    minutes = ("0" + Math.floor(time / 60)).slice(-2);
-    seconds = ("0" + (time % 60)).slice(-2);
-    document.querySelector(".timer").innerHTML = minutes + ":" + seconds;
-    }, 1000);
-}
-
-let clicked = flipCard;
-//Timer begins when opening the page
-if (timerOn === true) {
-    beginTimer();
-    timerOn = true;
-}
 //Reset the Game
 function reloadGame() {
   window.location.reload();
 }
 
-let startGame = function myFunction() {
+let resetGame = function myFunction() {
   reloadGame();
 };
+
+const info = document.getElementById('instruction-msg');
+
+//Closing window
+function closeWindow() {
+    info.style.display = 'none';
+    done.style.display = 'none';
+    gameLock = false;
+}    
